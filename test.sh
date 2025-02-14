aa871727c606fa204e78c9f4911998eab49aac8c
Matt Melling <mattmelling@fastmail.com>
2025-02-14 09:37:57 +0000

    disconnect all links before connecting to auditable node

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ NODE=$1
 LOCAL=596202
 ASTERISK="sudo asterisk -rx"
 
+$ASTERISK "rpt cmd $LOCAL ilink 6"
 $ASTERISK "rpt cmd $LOCAL ilink 3 $NODE"
 sleep 5
 RESULT=$($ASTERISK "rpt nodes $LOCAL" | grep -E "(T$NODE,|T$NODE$)" | wc -l)
