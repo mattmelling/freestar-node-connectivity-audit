c9e04605d12accc75c11cda90e9150768378142b
Matt Melling <mattmelling@fastmail.com>
2025-02-14 08:48:02 +0000

    fix connectivity check command

--- test.sh
+++ test.sh
@@ -4,11 +4,11 @@ NODE=$1
 LOCAL=596202
 ASTERISK="sudo asterisk -rx"
 
-$ASTERISK "rpt fun $LOCAL *3$NODE"
+$ASTERISK "rpt cmd $LOCAL ilink 3 $NODE"
 sleep 5
 RESULT=$($ASTERISK "rpt nodes $LOCAL" | grep -E "(T$NODE,|T$NODE$)" | wc -l)
 sleep 5
-$ASTERISK "rpt fun $LOCAL *1$NODE"
+$ASTERISK "rpt cmd $LOCAL ilink 1 $NODE"
 
 echo $NODE,$RESULT
 
